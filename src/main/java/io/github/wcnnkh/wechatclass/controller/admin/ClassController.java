5b77d8caded96590e2f0d891b09e80f741d16c45
舒超文 <shuchaowen@zx62580.com>
2022-03-14 14:33:48 +0800

    cleanup

--- src/main/java/io/github/wcnnkh/wechatclass/controller/admin/ClassController.java
+++ src/main/java/io/github/wcnnkh/wechatclass/controller/admin/ClassController.java
@@ -169,8 +169,9 @@ public class ClassController {
 
 	@RequestMapping(value = "downMessage")
 	public void downMessage(HttpServletRequest request, ServerHttpResponse httpServletResponse) throws Exception {
+		// TODO 这里是sqlite特有的语法datetime
 		datasource.export(new SimpleSql(
-				"select m.openid as 'openid', u.nickName as '昵称', datetime(m.cts, 'unixepoch', 'localtime') as '时间', m.type as '类型', m.`data` as '内容' from user as u,message as m where u.openid=m.openid"),
+				"select m.openid as 'openid', u.nickName as '昵称', datetime(m.cts/1000, 'unixepoch', 'localtime') as '时间', m.type as '类型', m.`data` as '内容' from user as u,message as m where u.openid=m.openid"),
 				new ExcelTemplate()).export(httpServletResponse, "聊天记录.xls");
 	}
 
